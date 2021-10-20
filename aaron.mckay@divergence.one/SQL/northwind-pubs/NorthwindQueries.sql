fea98fbb7886375e97d0e983ccdaecbe3f2099ab
cargostud <34919287+cargostud@users.noreply.github.com>
2021-10-19 21:18:10 -0600

    commit

--- aaron.mckay@divergence.one/SQL/northwind-pubs/NorthwindQueries.sql
+++ aaron.mckay@divergence.one/SQL/northwind-pubs/NorthwindQueries.sql
@@ -7,5 +7,12 @@ select CategoryName,Description from Categories;
 -- 3. Sales Representatives 
 select FirstName, LastName, HireDate from Employees where Title = 'Sales Representative';
 
--- 4. Sales Representatives from the US
+-- 4. Sales Representatives from the USA
 select FirstName, LastName, HireDate from Employees where Title = 'Sales Representative' and Country = 'USA';
+
+-- 5. Orders placed by specific EmployeeID
+select OrderID, OrderDate from Orders where EmployeeID = '5';
+
+-- 6. Suppliers and ContactTitles
+select SupplierID, ContactName, ContactTitle from Suppliers where ContactTitle <> 'Marketing Manager';
+
