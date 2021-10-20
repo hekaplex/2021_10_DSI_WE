1440ed2684b1bbafab1f0db6da44d06d8709588d
cargostud <34919287+cargostud@users.noreply.github.com>
2021-10-19 22:28:43 -0600

    commit

--- aaron.mckay@divergence.one/SQL/northwind-pubs/NorthwindQueries.sql
+++ aaron.mckay@divergence.one/SQL/northwind-pubs/NorthwindQueries.sql
@@ -16,3 +16,18 @@ select OrderID, OrderDate from Orders where EmployeeID = '5';
 -- 6. Suppliers and ContactTitles
 select SupplierID, ContactName, ContactTitle from Suppliers where ContactTitle <> 'Marketing Manager';
 
+-- 7. Products with "queso" in ProductName
+select ProductID, ProductName from Products where ProductName like '%queso%';
+
+-- 8. Orders shipping to France or Belgium
+select OrderID, CustomerID, ShipCountry from Orders where ShipCountry = 'France' OR ShipCountry = 'Belgium';
+select OrderID, CustomerID, ShipCountry from Orders where ShipCountry IN ('France', 'Belgium');
+-- Two different ways are shown that give the same result. Use either the 'IN' comparitor or the 'OR' comparitor
+
+-- 9. Orders shipping to any country in Latin America
+select OrderID, CustomerID, ShipCountry from Orders where ShipCountry IN ('Brazil', 'Mexico', 'Argentina', 'Venezuela'); 
+
+-- 10. Employees, in order of age with age ascending
+select firstname, lastname, title, birthdate from Employees order by birthdate ASC;  
+
+-- 11. 
